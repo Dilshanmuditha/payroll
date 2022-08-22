49ddb15503d331f4e52092484a0bd6d8d97ef21a
Dilshan Muditha <mudithadilshan1999@gmail.com>
2022-08-22 20:23:08 +0530

    Update DepartmentController.php

--- test/app/Http/Controllers/DepartmentController.php
+++ test/app/Http/Controllers/DepartmentController.php
@@ -3,29 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-//use illuminate\support\Facades\DB;
 use App\Models\Tbldepartment;
 
 class DepartmentController extends Controller
 {
-    //
     function showt(){
         $data = Tbldepartment::all();
-        //return view('depa',['section'=>$data]);
         return view('CreateNewEmployee',['section'=>$data]);
     }
-
-    /*function ShowDepartmentList(){
-        //return Tbldepartment::all();
-        $data = Tbldepartment::all();
-        //return view('ListDepartments',['item' =>$data]);
-        return view('CreateNewEmployee',['section'=>$data]);
-        //return view('CreateNewDeparts',['item' =>$data]);
-    }
-*/
 
     function SaveNewDepartsData(Request $req ){
-        //return $req->input();
         $TblDeparts = new Tbldepartment;
         $TblDeparts->dpartcode = $req -> DepCode;
         $TblDeparts->dpartname = $req -> DepName;
@@ -33,7 +20,7 @@ class DepartmentController extends Controller
         return redirect('NewDepartments');
     }
 
-    public function showDepartmentData(Tbldepartment $TblDeparts)
+    public function showDepartmentData()
     {
         return view('CreateNewDeparts',[
             'TblDeparts' => Tbldepartment::all(),
